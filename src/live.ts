285fa6d1c63f5562b04f977289793a4d2a478f7c
Xavier Brown <secton67@gmail.com>
2018-04-23 01:49:27 -0500

    add winston and lint live

--- src/live.ts
+++ src/live.ts
@@ -1,9 +1,10 @@
 import { app } from "./app";
 
-require("./server");
+import "./server";
+import { logger } from "./utils/log";
 
 const PORT = process.env.PORT || "4000";
 
 app.listen(PORT, () => {
-    console.log(`Nodeworld Live is now listening on localhost:${PORT}...`);
+    logger.info(`Nodeworld Live is now listening on localhost:${PORT}...`);
 });
---
+++ src/utils/log.ts
@@ -0,0 +1,9 @@
+import * as winston from "winston";
+
+export const logger = new winston.Logger({
+    transports: [
+        new winston.transports.Console({
+            colorize: true,
+        }),
+    ],
+});
